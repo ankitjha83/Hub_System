c64e45ae757d7ce1b090ba76f119fe50458d7a6f
ankitjha83 <67522896+ankitjha83@users.noreply.github.com>
2021-07-30 02:31:52 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import ScanQrcode from "./ScanQrcode/scanQrcode.js";
 import ScanBarcode from "./ScanBarcode/ScanBarcode.js";
 import GenerateBarcode from "./GenerateBarcode/generateBarcode";
+import GenerateQrcode from "./GenerateQrcode/GenerateQrcode";
 function App() {
   return (
     <div className="App">
       <br />
       <GenerateBarcode />
+      <br />
+      <GenerateQrcode />
       <ScanBarcode />
       <br />
       <br />
